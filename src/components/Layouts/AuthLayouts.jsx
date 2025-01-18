0c884d9e5fd10ab5bbd8ef260d0e83b0bec8c4d8
arikmhm <mhm.ariyanto@gmail.com>
2025-01-18 10:48:31 +0700

    conditional rendering

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+
 const AuthLayouts = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-xs">
@@ -10,9 +12,47 @@ const AuthLayouts = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+        {/* <p className="mt-4 text-gray-500 text-center">
+          {type === "login"
+            ? "Don&apos;t have an account?"
+            : "Already have an account?"}{" "}
+          {type === "login" && (
+            <Link to="/register" className="text-blue-500 font-semibold">
+              Register
+            </Link>
+          )}
+          {type === "register" && (
+            <Link to="/login" className="text-blue-500 font-semibold">
+              Login
+            </Link>
+          )}
+        </p> */}
+        <AuthNavigation type={type} />
       </div>
     </div>
   );
 };
 
+const AuthNavigation = ({ type }) => {
+  if (type === "login") {
+    return (
+      <p className="mt-4 text-gray-500 text-center">
+        {" "}
+        Don&apos;t have an account?{" "}
+        <Link to="/register" className="text-blue-500 font-semibold">
+          Register
+        </Link>
+      </p>
+    );
+  } else {
+    return (
+      <p className="mt-4 text-gray-500 text-center">
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-500 font-semibold">
+          Login
+        </Link>
+      </p>
+    );
+  }
+};
 export default AuthLayouts;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,10 @@
-import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/FormLogin";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
 const LoginPage = () => {
   return (
-    <AuthLayouts title="Login">
+    <AuthLayouts title="Login" type="login">
       <FormLogin />
-      <p className="mt-4 text-gray-500 text-center">
-        Don&apos;t have an account?{" "}
-        <Link to="/register" className="text-blue-500 font-semibold">
-          Register
-        </Link>
-      </p>
     </AuthLayouts>
   );
 };
